9584358bf6472c7034a14e285dc2cf1213cce7db
mikhaelrihani <mikabernik@gmail.com>
2023-08-24 00:14:06 +0200

    test uer controller et service nominatim

---
+++ src/Service/NominatimApiService.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Service;
+
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class NominatimService
+{
+
+    private $client;
+
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+
+    }
+
+    /**
+     * Return coordinates from nominatim Api
+     */
+    public function getCoordinates($city)
+    {
+
+        // déclenche une requête asynchrone
+        $response = $this->client->request(
+            // méthode htttp
+            'GET',
+            // url de l'api
+            'https://nominatim.openstreetmap.org/search',
+            [
+                // le paramètres ici est la recherche par ville
+                "query" => [
+                    "q" => $city
+                ]
+            ]
+        );
+
+        // je recupere toutes les donnees coordinates 
+        $cityAllCoordinates = $response->toArray();
+        // dd($cityAllCoordinates);
+        // je cree un tableau cityCoordinates pour y recuperer uniquement la latitude et longitude
+        $cityCoordinates = [];
+        $cityCoordinates[ "lat" ] = $cityAllCoordinates[ 0 ][ "lat" ];
+        $cityCoordinates[ "lon" ] = $cityAllCoordinates[ 0 ][ "lon" ];
+        // je retourne ce tableau sous format json
+
+        return json_encode($cityCoordinates);
+
+    }
+
+}
