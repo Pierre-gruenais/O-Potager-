d0581337a7fc97615532f98f4136b8efeb7f7d90
mikhaelrihani <mikabernik@gmail.com>
2023-08-23 14:40:44 +0200

    feat controller creation garden controller test

---
+++ src/Controller/GardenController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GardenController extends AbstractController
+{
+    /**
+     * @Route("/garden", name="app_api_garden")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/GardenController.php',
+        ]);
+    }
+}
