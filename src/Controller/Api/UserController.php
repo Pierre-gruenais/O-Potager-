0b338f2d123086dc1de71ce037a84d0c57c232a6
mikhaelrihani <mikabernik@gmail.com>
2023-08-24 12:23:55 +0200

    update UserController route getPost

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,15 +3,20 @@
 namespace App\Controller\Api;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/api/users", name="app_api_user_getUsers")
+     * @Route("/api/users", name="app_api_users_getUsers")
      */
     public function getUsers(UserRepository $userRepository): JsonResponse
     {
@@ -19,19 +24,67 @@ class UserController extends AbstractController
 
         return $this->json($users, Response::HTTP_OK, [], ["groups" => "users"]);
     }
-    
-   /**
-     * @Route("/api/users/{id}", name="app_api_user_getUsersById")
+
+    /**
+     * @Route("/api/users/{id}", name="app_api_users
+     * _getUsersById")
      */
     public function getUsersById(int $id, UserRepository $userRepository): JsonResponse
     {
 
         $user = $userRepository->find($id);
-            //  potentiellement j'ai une erreur si l'utilisateur n'existe pas
+        //  potentiellement j'ai une erreur si l'utilisateur n'existe pas
         if (!$user) {
             return $this->json(["error" => "l'utisateur n'esxiste pas"], Response::HTTP_BAD_REQUEST);
         }
         return $this->json($user, Response::HTTP_OK, [], ["groups" => "users"]);
     }
-    
+    /**
+     * @Route("/api/users", name="app_api_users_postUsers", methods={"POST"})
+     */
+    public function postUsers(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
+    {
+        // je récupere un json en brut
+        $jsonContent = $request->getContent();
+
+        // ! potentiellement j'ai une erreur si le json n'est pas bon
+        // je transforme ce json en entité user
+        try {
+            $user = $serializer->deserialize($jsonContent, User::class, 'json');
+        } catch (NotEncodableValueException $e) {
+            // ! je gere le cas ou il ya l'erreur
+            return $this->json(["error" => "JSON INVALID"], Response::HTTP_BAD_REQUEST);
+        }
+
+
+
+        // je detecte les erreurs sur mon entité avant de la persister
+        $errors = $validator->validate($user);
+
+        // on renvoi un json avec les erreurs
+        if (count($errors) > 0) {
+
+            // je crée un nouveau tableau d'erreur
+            $dataErrors = [];
+
+            foreach ($errors as $error) {
+                // j'injecte dans le tableau à l'index de l'input, les messages d'erreurs qui concernent l'erreur en question
+                $dataErrors[$error->getPropertyPath()][] = $error->getMessage();
+            }
+
+            // je retourne le json avec mes erreurs
+            return $this->json($dataErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $entityManager->persist($user);
+        dd($user);
+        $entityManager->flush();
+
+        return $this->json([$user], Response::HTTP_CREATED, [
+            "Location" => $this->generateUrl("app_api_users_getUsersById", ["id" => $user->getId()])
+        ], [
+                "groups" => "users"
+            ]);
+    }
+
 }
