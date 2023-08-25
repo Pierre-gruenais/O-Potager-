fcd229d4de70bdd8bf8125fc749cf3f433ae88b5
mikhaelrihani <mikabernik@gmail.com>
2023-08-26 01:25:57 +0200

    feat src création de eventsubscriber et exceptionsubscriber version corrigee

--- src/EventSubscriber/ApiExceptionSubscriber.php
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -2,42 +2,41 @@
 
 namespace App\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event): void
     {
-    
-       
-        // je récupère le requête 
         $request = $event->getRequest();
-        // si ma route ne commence pas par api, je fais un early return
-        if(strpos($request->getPathInfo(),"/api/")!== 0){
-            // ceci est un early return ça permet de couper l'execution de la fonction
+
+        // Check if the request path starts with /api/
+        if (strpos($request->getPathInfo(), "/api/") !== 0) {
             return;
         }
 
-        //  récupérer l'exception
         $exception = $event->getThrowable();
-        // le cas des erreurs serveur
-        if($exception->getCode() === 0){
-            return;
-        }
-        //  renvoyer une réponse avec en JSON le contenu du message ET le bon code http
-        $response = new JsonResponse(
-            ["error" => $exception->getMessage()],
-            $exception->getStatusCode()
-            
-        );
-        //! pour composer require symfony/http-kernel 
-        // ici je set la nouvelle réponse qui est personnalisé
-        $event->setResponse($response);
-        
 
+        // Handle HttpException
+        if ($exception instanceof HttpException) {
+            $response = new JsonResponse(
+                ["error" => $exception->getMessage()],
+                $exception->getStatusCode()
+            );
+        } else {
+            // Handle other exceptions with a default status code
+            $response = new JsonResponse(
+                ["error" => $exception->getMessage()],
+                JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        $event->setResponse($response);
     }
 
     public static function getSubscribedEvents(): array
