aacab2d3b3a134daf0afd48d77fc7ff787b6a219
mikhaelrihani <mikabernik@gmail.com>
2023-08-27 15:10:22 +0200

    fix app circular ref au niveua de user favorite

--- src/Entity/Favorite.php
+++ src/Entity/Favorite.php
@@ -15,7 +15,7 @@ class Favorite
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     *@Groups({"userWithRelations"})
+     *@Groups({"usersWithRelations"})
      */
     private $id;
 
